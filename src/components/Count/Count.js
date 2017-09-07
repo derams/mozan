a0d8a37e3590c0bae57880516a4e9f47dff0a4a0
derams <1281666753@qq.com>
2017-09-07 06:21:25 +0000

    22

---
+++ src/components/Count/Count.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Count extends Component {
+  render(){
+    return(
+      <div className="">
+
+      </div>
+    )
+  }
+}
+
+export default Count
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import Banner from '../Banner/Banner.js'
+import Count from '../Count/Count.js'
+
+
 class Home extends Component {
   render(){
     return(
       <div className="home">
         <Banner/>
+        <Count/>
       </div>
     )
   }
